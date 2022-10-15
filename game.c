eb6659ec21f8ec96c28bff6bcce13fc48ad72c2c
Sam Willems <swi126@uclive.ac.nz>
2022-10-15 16:18:34 +1300

    include led.h

--- game.c
+++ game.c
@@ -1,13 +1,18 @@
 #include "system.h"
+#include "led.h"
+
+// Defining loop rate in Hz.
+#define LOOP_RATE 100
 
 int main (void)
 {
     system_init ();
+    led_init();
 
 
     while (1)
     {
-
+        led_set(1, 1);
 
 
     }
