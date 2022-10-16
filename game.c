2370a974426f4df7c67cfee743ab4b9e87edbbe4
Zachary Avis <zav10@uclive.ac.nz>
2022-10-16 05:52:27 +0000

    Updating to add nav.h move

--- game.c
+++ game.c
@@ -3,8 +3,9 @@
 #include "navswitch.h"
 #include "ir_uart.h"
 #include "tinygl.h"
+#include "nav.h"
 #include "../fonts/font5x7_1.h"
-#include "system.h"
+#include "button.h"
 #include "pio.h"
 #include <stdlib.h>
 
@@ -63,6 +64,24 @@ static void clear_screen(void) {
     }
 }
 
+static int arrow_speed = 60;
+
+static void button_task_init(void)
+{
+    button_init();
+}
+
+
+static void button_task(void)
+{
+    button_update();
+
+    if (button_push_event_p(BUTTON1))
+    {
+        arrow_speed = arrow_speed - (arrow_speed / 10);
+    }
+}
+
 
 int main(void)
 {
@@ -73,24 +92,32 @@ int main(void)
 
     /* Initialize  */
     pacer_init(LOOP_RATE);
+    button_task_init();
 
     int delay = 1000;
     int current_tick = 0;
-    int arrow_speed = 30;
 
     int arrow_x = 0;
     int arrow_y = 0;
 
+    int correctCount = 0;
+
     while (1)
     {
+        printf("%d", correctCount);
+        button_task();
+
         /* Pace the loop.  */
         pacer_wait();
         clear_screen();
+        navswitch_update();
+
         current_tick++;
 
         if (current_tick % arrow_speed == 0) {
             arrow_y++;
         }
+
         if (arrow_y >= 7) {
             arrow_y = 0;
             arrow_x = rand() % 5;
@@ -101,5 +128,11 @@ int main(void)
         }
 
         ledmat_pixel_set(arrow_x, arrow_y, 1);
+        // nav.h
+        move(arrow_x, arrow_y, &correctCount);
+
+        if (correctCount == 2)
+            led_set(LED1, 1);
+
     }
 }
