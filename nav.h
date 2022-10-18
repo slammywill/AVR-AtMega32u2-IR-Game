7c822c813eb3a4f9749c5a31855f6cb455db5227
Zachary Avis <zav10@uclive.ac.nz>
2022-10-18 00:21:11 +0000

    Added desc & int x

--- nav.h
+++ nav.h
@@ -1,8 +1,14 @@
+/*
+* HEADER FILE
+* AUTHORS: Zac Avis, Sam Willems
+* DESC: nav.c header file
+*/
+
 #ifndef NAV_H
 #define NAV_H
-
 #include "system.h"
 
+int x;
 void move(int, int, int*, int*, int*);
 
 #endif
