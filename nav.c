86adb706d65a7fb4b7637dbde97c5093e20055c5
Zachary Avis <zav10@uclive.ac.nz>
2022-10-18 00:20:50 +0000

    Added; one move per arrow, and if player does nothing then it'll count as a wrong move

--- nav.c
+++ nav.c
@@ -4,15 +4,19 @@
 
 #include "nav.h"
 
+// VARIABLES
+static int moveDone = 0;
+
 // CHECKS TO SEE IF THE PLAYER HAS MADE A RIGHT MOVE OR A WRONG MOVE & ALSO 
 // CHECKS TO SEE HOW MANY WRONG MOVES THE PLAYER HAS MADE IN A ROW
 void move(int arrow_x, int arrow_y, int* correctCount, int* wrongMove, int* score) {
 
     // VARIABLES
-    int y_check = 1;
+    int y_check = 6;
 
     // LEFT
-    if (navswitch_push_event_p(NAVSWITCH_WEST)) {
+    if (navswitch_push_event_p(NAVSWITCH_WEST) && moveDone == 0) {
+        moveDone++;
         if (arrow_y >= y_check && arrow_x == 0) {
             (*correctCount)++;
             (*score)++;
@@ -24,7 +28,8 @@ void move(int arrow_x, int arrow_y, int* correctCount, int* wrongMove, int* scor
         }
     }
     // LEFT-MIDDLE
-    else if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
+    else if (navswitch_push_event_p(NAVSWITCH_NORTH) && moveDone == 0) {
+        moveDone++;
         if (arrow_y >= y_check && arrow_x == 1) {
             (*correctCount)++;
             (*score)++;
@@ -37,7 +42,8 @@ void move(int arrow_x, int arrow_y, int* correctCount, int* wrongMove, int* scor
     }
 
     // MIDDLE
-    else if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+    else if (navswitch_push_event_p(NAVSWITCH_PUSH) && moveDone == 0) {
+        moveDone++;
         if (arrow_y >= y_check && arrow_x == 2) {
             (*correctCount)++;
             (*score)++;
@@ -50,7 +56,8 @@ void move(int arrow_x, int arrow_y, int* correctCount, int* wrongMove, int* scor
     }
 
     // RIGHT-MIDDLE
-    else if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
+    else if (navswitch_push_event_p(NAVSWITCH_SOUTH) && moveDone == 0) {
+        moveDone++;
         if (arrow_y >= y_check && arrow_x == 3) {
             (*correctCount)++;
             (*score)++;
@@ -61,8 +68,10 @@ void move(int arrow_x, int arrow_y, int* correctCount, int* wrongMove, int* scor
             (*wrongMove)++;
         }
     }
+
     // RIGHT
-    else if (navswitch_push_event_p(NAVSWITCH_EAST)) {
+    else if (navswitch_push_event_p(NAVSWITCH_EAST) && moveDone == 0) {
+        moveDone++;
         if (arrow_y >= y_check && arrow_x == 4) {
             (*correctCount)++;
             (*score)++;
@@ -72,5 +81,16 @@ void move(int arrow_x, int arrow_y, int* correctCount, int* wrongMove, int* scor
             *correctCount = 0;
             (*wrongMove)++;
         }
+    }
+
+    // IF ARROW TOUCHES BOTTOM ROW & PLAYER HASN'T MADE A MOVE
+    else if (moveDone == 0 && arrow_y >= 7) {
+        *correctCount = 0;
+        (*wrongMove)++;
+    }
+
+    // RESET
+    if (moveDone == 1 && arrow_y >= 7) {
+        moveDone = 0;
     }
 }
